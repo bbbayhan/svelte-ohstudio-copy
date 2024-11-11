a45a6ea1b98590650570a7589cb5259f46f2a338
bbbayhan <bestebbayhan@gmail.com>
2024-11-11 23:05:13 +0100

    Extract home component

---
+++ src/components/home.svelte
@@ -0,0 +1,9 @@
+<section class="mx-72">
+	<h1 class="text-center text-7xl">A brand and product designer working with clients globally</h1>
+</section>
+<section class="mx-12 my-32 grid grid-cols-2 gap-4">
+	<img class="h-96 w-full rounded-xl object-cover" alt="coffee" src="./img1.jpg" />
+	<img class="h-96 w-full rounded-xl object-cover" alt="flower" src="./img2.jpg" />
+	<img class="h-96 w-full rounded-xl object-cover" alt="sofa" src="./img3.jpg" />
+	<img class="h-96 w-full rounded-xl object-cover" alt="table" src="./img4.jpg" />
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,36 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import Home from '../components/home.svelte';
+
+	let selectedTab = $state('0');
+	function selectTab(id: string) {
+		selectedTab = id;
+		return;
+	}
+</script>
+
+<div class="flex flex-col items-center">
+	<div class="mt-8 flex justify-around rounded-full bg-gray-200 px-1 py-1">
+		<button
+			class={`px-8 py-4 ${selectedTab === '0' ? 'bg-white' : ''} rounded-full`}
+			onclick={() => selectTab('0')}>Home</button
+		>
+		<button
+			class={`px-8 py-4 ${selectedTab === '1' ? 'bg-white' : ''} rounded-full`}
+			onclick={() => selectTab('1')}>Profile</button
+		>
+		<button
+			class={`px-8 py-4 ${selectedTab === '2' ? 'bg-white' : ''} rounded-full`}
+			onclick={() => selectTab('2')}>Contact</button
+		>
+	</div>
+
+	<div class="mt-16">
+		{#if selectedTab === '0'}
+			<Home />
+		{/if}
+		{#if selectedTab === '1'}
+			<div>ProfileContent</div>{/if}
+		{#if selectedTab === '2'}
+			<div>ContactContent</div>{/if}
+	</div>
+</div>
